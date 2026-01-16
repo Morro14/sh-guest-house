89b4dcefdc92325d3851ed06a3f9061c76990cfb
Ivan Fomin <ivfmn1@gmail.com>
2026-01-16 12:21:04 +0400

    fix carousel dots full view

--- client/app/app/components/carousel/CarouselDotsFullView.tsx
+++ client/app/app/components/carousel/CarouselDotsFullView.tsx
@@ -1,46 +1,83 @@
-import type { EmblaViewportRefType } from "embla-carousel-react"
-import { useEffect, useRef, useState } from "react"
-import { useNavContextProvider } from "../nav/NavContextProvider"
-import NavArrow from "app/components/nav/NavArrow"
+import type { EmblaViewportRefType } from "embla-carousel-react";
+import { useEffect, useRef, useState } from "react";
+import { useNavContextProvider } from "../nav/NavContextProvider";
+import NavArrow from "app/components/nav/NavArrow";
 
-
-
-export default function CarouselDotsFullView({ emblaRef, emblaApi, snapListLen }: { emblaRef: EmblaViewportRefType, emblaApi: any, snapListLen: number }) {
-  const [selectedIndex, setSelectedIndex] = useState(0)
-  const context = useNavContextProvider()
-  const selectorRef = useRef(undefined)
+export default function CarouselDotsFullView({
+  emblaRef,
+  emblaApi,
+  snapListLen,
+}: {
+  emblaRef: EmblaViewportRefType;
+  emblaApi: any;
+  snapListLen: number;
+}) {
+  const context = useNavContextProvider();
+  const [dotSelected, setDotSelected] = useState(context.imageSelected);
+  console.log("imageSelected", context.imageSelected);
+  const selectorRef = useRef(undefined);
   if (selectorRef.current) {
-    selectorRef.current.style.left = String(selectedIndex * 35 + 'px')
+    selectorRef.current.style.left = String(dotSelected * 35 + "px");
   }
-  const [dots, setDots] = useState<any>([])
-  // console.log("full view dots", dots)
+  const [dots, setDots] = useState([]);
   useEffect(() => {
     if (!emblaApi) {
-      return
+      return;
     }
     if (emblaApi) {
-      const snapsLength = snapListLen
-      console.log('snapsLength', snapsLength)
-      const genMoreDots = (newDotsNum: number, currentDotsNum: number) => Array.from({ length: newDotsNum }, (_, i) => {
-        return <div key={`room-dot-${currentDotsNum + i}`} onClick={() => emblaApi?.scrollTo(currentDotsNum + i)} className="w-3.5 h-3.5 rounded-[7px] cursor-pointer hover:bg-peach-light bg-gray-warm"></div>
-      })
-      const genDots = genMoreDots(snapsLength, 0)
-      setDots(genDots)
+      setDotSelected(emblaApi.selectedScrollSnap());
+      const snapsLength = snapListLen;
+      const genMoreDots = (newDotsNum: number, currentDotsNum: number) =>
+        Array.from({ length: newDotsNum }, (_, i) => {
+          return (
+            <div
+              key={`room-dot-${currentDotsNum + i}`}
+              onClick={() => emblaApi?.scrollTo(currentDotsNum + i)}
+              className="w-3.5 h-3.5 rounded-[7px] cursor-pointer hover:bg-peach-light bg-gray-warm"
+            ></div>
+          );
+        });
+      const genDots = genMoreDots(snapsLength, 0);
+      setDots(genDots);
     }
     emblaApi.on("select", () => {
-      setSelectedIndex(emblaApi.selectedScrollSnap())
-    })
-  }, [emblaApi])
-  console.log('dots', emblaApi?.selectedScrollSnap())
+      setDotSelected(emblaApi.selectedScrollSnap());
+    });
+  }, [snapListLen, emblaApi]);
 
-  return <div className={"flex grow gap-[41px] justify-center items-center transtion-all duration-300"}>
-    <NavArrow key={'rooms-arrow-left'} direction="left" numElements={dots.length} index={selectedIndex} func={() => selectedIndex > 0 ? emblaApi?.scrollTo(selectedIndex - 1) : undefined} />
-    <div className="flex relative gap-[21px]">
-      <div ref={selectorRef} className="absolute w-3.5 h-3.5 rounded-[7px] bg-peach transition-all duration-500 ease-out pointer-event-none"></div>
-      {dots ? dots : ""}
+  return (
+    <div
+      className={
+        "flex grow gap-[41px] justify-center items-center transtion-all duration-300"
+      }
+    >
+      <NavArrow
+        key={"rooms-arrow-left"}
+        direction="left"
+        numElements={dots.length}
+        index={dotSelected}
+        func={() =>
+          dotSelected > 0 ? emblaApi?.scrollTo(dotSelected - 1) : undefined
+        }
+      />
+      <div className="flex relative gap-[21px]">
+        <div
+          ref={selectorRef}
+          className="absolute w-3.5 h-3.5 rounded-[7px] bg-peach transition-all duration-500 ease-out pointer-event-none"
+        ></div>
+        {dots ? dots : ""}
+      </div>
+      <NavArrow
+        key={"rooms-arrow-right"}
+        direction="right"
+        index={dotSelected}
+        func={() =>
+          dotSelected < dots.length - 1
+            ? emblaApi?.scrollTo(dotSelected + 1)
+            : undefined
+        }
+        numElements={dots.length}
+      />
     </div>
-    <NavArrow key={'rooms-arrow-right'} direction="right" index={selectedIndex} func={() => selectedIndex < dots.length - 1 ? emblaApi?.scrollTo(selectedIndex + 1) : undefined} numElements={dots.length} />
-
-  </div>
+  );
 }
-
--- client/app/app/components/nav/Nav.tsx
+++ client/app/app/components/nav/Nav.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import NavLink from "./NavLink.tsx";
 import NavScrollArrow from "./NavScrollArrow.tsx";
 import { useNavContextProvider } from "./NavContextProvider.tsx";
@@ -7,43 +7,67 @@ type NavProps<T> = {
   items: T[];
   template: React.ComponentType<{ item: T }>;
   slug: string;
-  contextProvider: () => NavContextValue
-
-}
+  contextProvider: () => NavContextValue;
+};
 type NavContextValue = {
   itemSelected: number;
   setItemSelected: (i: number) => void;
   lastSelected: number;
-  setLastSelected: (i: number) => void
-}
-export default function Nav<T>({ slug, items, contextProvider, template: NavLinkTemplate }: NavProps<T>) {
-  const context = useNavContextProvider()
-  const selectorRef = useRef<HTMLDivElement>(undefined)
-  const [scrollRefState, setScrollRefState] = useState({ scrollTop: 0, scrollTopMax: 0 })
-  const scrollRef = useRef<HTMLDivElement>(null)
+  setLastSelected: (i: number) => void;
+};
+export default function Nav<T>({
+  slug,
+  items,
+  template: NavLinkTemplate,
+}: NavProps<T>) {
+  const context = useNavContextProvider();
+  const selectorRef = useRef<HTMLDivElement>(undefined);
+  const [scrollRefState, setScrollRefState] = useState({
+    scrollTop: 0,
+    scrollTopMax: 0,
+  });
+  const scrollRef = useRef<HTMLDivElement>(null);
   // const { scrollPos } = useFloatingSelector(context.lastSelected, loaded, context.itemSelected, scrollRef, selectorRef)
-  if (selectorRef.current) { selectorRef.current.style.top = String(context.selectorPos * selectorRef.current.clientHeight) + 'px' }
-
+  if (selectorRef.current) {
+    selectorRef.current.style.top =
+      String(context.selectorPos * selectorRef.current.clientHeight) + "px";
+  }
 
   return (
     <div className="flex flex-col items-center relative">
-      <NavScrollArrow direction="up" scrollPos={scrollRefState} scrollEl={scrollRef?.current}></NavScrollArrow>
-      <div ref={scrollRef} onScroll={() => setScrollRefState({ scrollTop: scrollRef.current.scrollTop, scrollTopMax: scrollRef.current.scrollTopMax })} className="scroll-smooth overflow-scroll relative flex flex-col 2xl:w-[296px] 2xl:h-[380px]">
-        <div className={`absolute w-[2px] h-[70px] bg-peach transition-all ease-out`} ref={selectorRef}></div>
+      <NavScrollArrow
+        direction="up"
+        scrollPos={scrollRefState}
+        scrollEl={scrollRef?.current}
+      ></NavScrollArrow>
+      <div
+        ref={scrollRef}
+        onScroll={() =>
+          setScrollRefState({
+            scrollTop: scrollRef.current.scrollTop,
+            scrollTopMax: scrollRef.current.scrollTopMax,
+          })
+        }
+        className="scroll-smooth overflow-scroll relative flex flex-col 2xl:w-[296px] 2xl:h-[380px]"
+      >
+        <div
+          className={`absolute w-[2px] h-[70px] bg-peach transition-all ease-out`}
+          ref={selectorRef}
+        ></div>
         {items.map((item, i) => {
           return (
-            <NavLink
-              key={`${slug}-nav-link-${i}`}
-              index={i}
-              context={context}
-            >
+            <NavLink key={`${slug}-nav-link-${i}`} index={i} context={context}>
               <NavLinkTemplate item={item}></NavLinkTemplate>
             </NavLink>
           );
         })}
       </div>
       {/* <div className={`${scrollRef?.current.scrollTop !== scrollRef?.current.scrollTopMax ? "h-[2px] bg-peach" : ""} w-full`}></div> */}
-      <NavScrollArrow direction="down" scrollPos={scrollRefState} scrollEl={scrollRef?.current}></NavScrollArrow>
+      <NavScrollArrow
+        direction="down"
+        scrollPos={scrollRefState}
+        scrollEl={scrollRef?.current}
+      ></NavScrollArrow>
     </div>
   );
 }
