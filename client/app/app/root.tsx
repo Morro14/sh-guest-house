ec9f559ac17f1b21a164de9ff8cbc4003d19ee9f
Ivan Fomin <ivfmn1@gmail.com>
2026-03-17 19:58:45 +0400

    change render env var

--- client/app/app/root.tsx
+++ client/app/app/root.tsx
@@ -7,35 +7,46 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="/static/frontend/favicon.ico"
+        />
 
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/static/frontend/favicon-16x16.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/static/frontend/favicon-32x32.png"
         />
 
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/static/frontend/apple-touch-icon.png"
         />
 
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/static/frontend/site.webmanifest" />
         <meta charSet="utf-8" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, minimum-scale=1"
         />
+        <meta
+          name="description"
+          content="A demo version of a guest-house website"
+        />
+
         <Links />
+        <title>Shushan GH</title>
       </head>
+
       <body>
         {children}
         <ScrollRestoration />
--- client/app/vite.config.ts
+++ client/app/vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/static/frontend/",
+  base: "/static/frontend/",
   plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
   resolve: {
     alias: {
