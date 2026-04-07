a3b00eba05a37ccfe78086e498197b426fe30848
Ivan Fomin <ivfmn1@gmail.com>
2026-04-07 22:15:45 +0400

    add map move limits

--- client/app/app/components/index/map/Map.tsx
+++ client/app/app/components/index/map/Map.tsx
@@ -16,6 +16,7 @@ export default function Map() {
     : null;
   const mapSurface = useRef<HTMLDivElement | null>(null);
   const mapContainer = useRef<HTMLDivElement | null>(null);
+
   useEffect(() => {
     if (!mapSurface.current || !mapContainer.current) {
       return;
@@ -31,15 +32,50 @@ export default function Map() {
       ),
     };
     // setMapOffset({ x: initOffset.x, y: initOffset.y });
+    mapSurface.current.style.left = `${-initOffset.x}px`;
+    mapSurface.current.style.top = `${-initOffset.y}px`;
     let mouseDownX: number, mouseDownY: number;
-    let mapOffsetX: number = 0,
-      mapOffsetY: number = 0;
+    let mapOffsetX: number = -initOffset.x;
+    let mapOffsetY: number = -initOffset.y;
     // const initPos = { x: mapOffset.x, y: mapOffset.y };
+    //
+    const thresholds = {
+      top: 0,
+      bottom:
+        -mapSurface.current.clientHeight + mapContainer.current.clientHeight,
+      left: 0,
+      right: -mapSurface.current.clientWidth + mapContainer.current.clientWidth,
+    };
     const move = (e: MouseEvent) => {
       // console.log("mouse down", mouseDownX, mouseDownY);
       // console.log(mapSurface.current);
-      mapSurface.current.style.left = `${mapOffsetX + e.clientX - mouseDownX}px`;
-      mapSurface.current.style.top = `${mapOffsetY + e.clientY - mouseDownY}px`;
+      const limits = {
+        top: mapSurface.current.offsetTop > thresholds.top,
+        bottom: mapSurface.current.offsetTop < thresholds.bottom,
+        left: mapSurface.current.offsetLeft > thresholds.left,
+        right: mapSurface.current.offsetLeft < thresholds.right,
+      };
+      const allowedMove = {
+        top: mouseDownY > e.clientY || !limits.top,
+        bottom: mouseDownY > e.clientY || !limits.bottom,
+        left: mouseDownX > e.clientX || !limits.left,
+        right: mouseDownX < e.clientX || !limits.right,
+      };
+      if (!allowedMove.left) {
+        mapSurface.current.style.left = `0px`;
+      } else if (!allowedMove.right) {
+        mapSurface.current.style.left = `${mapContainer.current.clientWidth - mapSurface.current.clientWidth}px`;
+      } else {
+        mapSurface.current.style.left = `${mapOffsetX + e.clientX - mouseDownX}px`;
+      }
+
+      if (!allowedMove.top) {
+        mapSurface.current.style.top = `0px`;
+      } else if (!allowedMove.bottom) {
+        mapSurface.current.style.top = `${mapContainer.current.clientHeight - mapSurface.current.clientHeight}px`;
+      } else {
+        mapSurface.current.style.top = `${mapOffsetY + e.clientY - mouseDownY}px`;
+      }
     };
     mapSurface.current.addEventListener("mousedown", (e) => {
       mouseDownX = e.clientX;
@@ -61,7 +97,7 @@ export default function Map() {
       className="index-container-1 relative h-[md:1180px] overflow-clip border border-text-main"
       ref={mapContainer}
     >
-      <div className="w-300 h-400 relative bg-olive-400" ref={mapSurface}>
+      <div className="w-500 h-500 relative bg-olive-400" ref={mapSurface}>
         {placesObj ? <MapPlace place={placesObj["spitakavor"]}></MapPlace> : ""}
       </div>
     </div>
