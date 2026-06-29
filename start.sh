5b1286a14d1129f641ccae9392c7146db364a3d1
Ivan Fomin <ivfmn1@gmail.com>
2026-06-29 19:48:03 +0400

    add copying stage migrations from persistant disk to start.sh script

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 sh -c "mkdir -p /var/data/db &&
   cd ./server/app/ &&
+  cp -r /var/data/db/migrations/site_content/* ./site_content/migrations/ &&
+  cp -r /var/data/db/migrations/main/* ./main/migrations/ &&
+  cp -r /var/data/db/migrations/auth_app/* ./auth_app/migrations/ &&
   python manage.py makemigrations &&
   python manage.py migrate &&
   python manage.py createsuperuser --no-input || true &&
