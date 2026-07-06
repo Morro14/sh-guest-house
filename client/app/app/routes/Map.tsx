44643383f116b709330adb8feecf518a4514181d
Ivan Fomin <ivfmn1@gmail.com>
2026-07-06 15:53:09 +0400

    remove pinchZoom.ts old impl

--- client/app/app/routes/Map.tsx
+++ client/app/app/routes/Map.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import Map from "~/components/index/map/Map";
 import MapContextProvider from "~/components/index/map/MapContextProvider";
+import openMapPic from "src/assets/open-map-pic.svg";
 
 export default function MapPage() {
   const { t } = useTranslation();
+  const [mapFullView, setMapFullView] = useState(false);
   return (
     <div className="w-screen flex justify-center">
-      <div className="index-container-1">
+      <div className="index-container-1 flex flex-col items-center">
         <div
           id="points-of-interest"
           className="flex flex-col gap-6 relative scroll-mt-15 my-8"
@@ -18,9 +21,18 @@ export default function MapPage() {
             </h3>
           </div>
         </div>
-        <MapContextProvider>
-          <Map></Map>
-        </MapContextProvider>
+        {!mapFullView ? (
+          <div className="flex justify-center items-center w-[238px] h-[238px] border border-gray-300">
+            <button className="underline font-serif font-bold text-lg px-2 py-2 bg-bg z-10">
+              {t("Open map")}
+            </button>
+            <img src={openMapPic} className="absolute " />
+          </div>
+        ) : (
+          <MapContextProvider>
+            <Map></Map>
+          </MapContextProvider>
+        )}
       </div>
     </div>
   );
