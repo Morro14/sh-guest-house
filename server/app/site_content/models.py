7afc32ca347920fd25a6653d0f5dbdd0f5256268
Ivan Fomin <ivfmn1@gmail.com>
2026-08-05 16:20:05 +0400

    change blur field to tiny

--- server/app/site_content/models.py
+++ server/app/site_content/models.py
@@ -89,12 +89,8 @@ def get_upload_path(instance, filename):
             )
 
 
-class Test(models.Model):
-    image = models.ImageField(upload_to="test/")
-
-
 class ImageBase(models.Model):
-    blur_res = (100, 100)
+    tiny_res = (100, 100)
     small_res = (600, 600)
     main_res = (1280, 1280)
 
@@ -103,7 +99,7 @@ class ImageBase(models.Model):
     image_full = models.ImageField(upload_to=get_upload_path)
     cropping_main = ImageRatioField("image_full", size_to_str(main_res))
     cropping_small = ImageRatioField("image_full", size_to_str(small_res))
-    cropping_blur = ImageRatioField("image_full", size_to_str(blur_res))
+    cropping_tiny = ImageRatioField("image_full", size_to_str(tiny_res))
 
     def get_variant_url(self, size, box=None, quality=80, blur=False):
         options = {
@@ -135,7 +131,7 @@ class ImageBase(models.Model):
         else:
             original_pathname = self.image_full.url
         results = {
-            "blur": self.get_variant_url(self.blur_res, blur=True),
+            "tiny": self.get_variant_url(self.tiny_res),
             "small": self.get_variant_url(self.small_res),
             "main": self.get_variant_url(self.main_res),
             "original": original_pathname,
@@ -265,7 +261,6 @@ class ImageTag(ImageTagBase):
 
 class RoomImage(Image):
     small_res = (700, 0)
-    blur_res = (20, 12)
 
     class Meta:
         verbose_name = _("room image")
