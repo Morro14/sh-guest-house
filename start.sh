a0bc4c1d90a81cb755fa376b51961335cfe28823
Ivan Fomin <ivfmn1@gmail.com>
2026-08-05 16:27:08 +0400

    add makemigrations to start script

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 sh -c "mkdir -p /var/data/db &&
   cd ./server/app/ &&
+  python manage.py makemigrations &&
   python manage.py migrate &&
   python manage.py createsuperuser --no-input || true &&
   python manage.py set_site &&
